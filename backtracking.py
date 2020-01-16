a9ffac99c0ce6f15860b54d0e40d72b81cb8095e
nmndeep <nmndeep@gmail.com>
2020-01-16 23:12:47 +0100

    With stopping criterion

--- backtracking.py
+++ backtracking.py
@@ -9,23 +9,29 @@ import numpy as np
 import scipy.io as sio
 
                                                     ################      Data loading    ################
+mat_contents = sio.loadmat('MNIST-TrainTestNonBinary.mat')
+mat_contents.keys()
+X_trai = mat_contents['Xtrain']
+y_train = mat_contents['Ytrain']
+X_test = mat_contents['Xtest']
+y_test = mat_contents['Ytest']
 
-X_train = "load data"
-X_test = "load test data"
+X_train = np.hstack((np.array(X_trai),np.array(y_train)))
+X_test = np.hstack((np.array(X_test),np.asarray(y_test)))
 np.random.shuffle(X_train)
 
 y_train = X_train[:,-1]
 y_test = X_test[:,-1]
 X_train = X_train[:,:-1]
 
-X = X_train[0:1500, :]
-y = np.array(y_train[0:1500],dtype=np.int32)
+X = X_train[:, :]
+y = np.array(y_train,dtype=np.int32)
 
 
 print("MNIST data using backtracking linesearch")
 print("--"*20)
 
-batch_size = 32
+batch_size = 100
 n_classes = 10
 
                     ###################    Gradients and loss calculation   ############
@@ -60,40 +66,44 @@ def backprop(X, y, W, lamb):
 
     return L, grad_W
                                 ########   Paramaters and init    #############
-alpha = 0.5
-beta = 0.8      ###    For backtracking
+alpha = 0.4
+beta = 0.6      ###    For backtracking
 W = np.zeros(shape=(784, n_classes))
-lamb = 1e-2   ### lambda in weight regularizer
+lamb = 0   ### lambda in weight regularizer
 t = 1
 
                                 #########     Function for backtracking rule        ############
 
 def backtrack(W, grad_W, loss, alpha, X, y, lamb, t):
     count = 1
-    loss_val,_ = backprop(X, y, W-t*grad_W, lamb)
-    while loss_val > loss-(t*alpha)*np.sum(grad_W**2):
+    loss_val,_ = backprop(X, y, W-t*grad_W, lamb)    #####     Function value at x-t*x'
+    while loss_val <= loss-(t*alpha)*np.square(np.linalg.norm(grad_W, 'fro')):              ########      f(x-tx') > f(x) - t*alpha*|x'|^2
         t *= beta
     return t
                                 #################    Paramaters     ############################
-epoch = 5
-batch_size = 32
-start = 0
-lamb = 1e-2
+batch_size = 100
 
                                 ###############     Training with GD and backtracking      #########
+start=0
+alpha = 1.0
+count = 1
+grad_W = np.zeros_like(W)
+t = 1
+                              #################      Condition either full run of training data or ||grad_W||<1e-3      ##########
+while np.linalg.norm(grad_W, 'fro') < 1e-3:
+    start = 0
+    while start < 60000-batch_size :
+        loss, grad_W = backprop(X[start:start+batch_size,:], y[start:start+batch_size], W, lamb)
+        alpha = backtrack(W, grad_W, loss, alpha, X[start:start+batch_size,:], y[start:start+batch_size], lamb, t)
+        # count+=1
+        t = alpha
+        W = W - alpha*grad_W
+        start+=batch_size
 
-while start < 600-batch_size:
-    t = 1
-    loss, grad_W = backprop(X[start:start+batch_size,:], y[start:start+batch_size], W, lamb)
-    alpha = backtrack(W, grad_W, loss, alpha, X[start:start+batch_size,:], y[start:start+batch_size], lamb, t)
-    W = W-alpha*grad_W
-    start+=batch_size
-
-# val = np.zeros(shape=[550])
-                              ###############        Test your model     ###########
-corr = 0
-predicted = np.argmax(np.dot(X[0:500,:],W))
-result = numpy.where(predicted==y[0:predicted.size])
-print(result.size)
-
-print("Number of correct, wrong and percent",result.size, 500-result.size,result.size/(500))
+######        Training  and Test Accuracy      ###########
+predicted = np.argmax(np.dot(X,W), axis = 1)
+result = np.where(predicted==y[0:predicted.size])
+print("Number of correct, wrong and percentage for trainig data",result[0].size, predicted.size-result[0].size,result[0].size/predicted.size)
+predicted = np.argmax(np.dot(X_test[:,:-1],W), axis = 1)
+result = np.where(predicted==y_test[0:predicted.size])
+print("Number of correct, wrong and percentage for test data",result[0].size, predicted.size-result[0].size,result[0].size/predicted.size)
